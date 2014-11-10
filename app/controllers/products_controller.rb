25c73fb0b13f70248687c7c87dd814cd2bbf9bba
Jude Fiorillo <jude.fiorillo@gmail.com>
2014-11-10 14:29:45 -0500

    Create form to add products

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,48 @@
+class ProductsController < ApplicationController
+  before_action :load_product, only: [:show, :edit, :update, :destroy]
+
+  def index
+  	@products = Product.all 
+  end
+
+  def show
+  end
+
+  def new
+  	@product = Product.new
+  end
+
+  def create
+  	@product = Product.new(product_params)
+  	if @product.save
+  		redirect_to products_path
+  	else
+  		render :new
+  	end
+  end
+
+  def edit
+  end
+
+  def update
+  	if @product.update_attributes(product_params)
+  		redirect_to "/products/#{@product.id}"
+  	else
+  		render :edit
+  	end
+  end
+
+  def destroy
+  	@product.destroy
+  	redirect_to products_path
+  end
+
+  private
+  def product_params
+  	params.require(:product).permit(:name, :description, :price_in_cents)
+  end
+
+  def load_product
+  	@product = Product.find(params[:id])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+
 resources :products
 # get 'products'             => 'products#index'
 # post 'products'            => 'products#create'
